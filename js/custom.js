fd79bc6c0e6ef85b67278f5bd3df06cfc0610199
jainal09 <jainal09gosalia@gmail.com>
2019-03-13 12:43:45 +0530

    major elements added

--- js/custom.js
+++ js/custom.js
@@ -195,27 +195,24 @@
         $container.masonry();
     });
 
-    // Back to Top
-    if ( $( '.back-to-top' ).length) {
-        var scrollTrigger = 500, // px
-            backToTop = function () {
-                var scrollTop = $(window).scrollTop();
-                if (scrollTop > scrollTrigger) {
-                    $('.back-to-top').addClass('show');
-                } else {
-                    $('.back-to-top').removeClass('show');
-                }
-            };
-        backToTop();
-        $(window).on('scroll', function () {
-            backToTop();
-        });
-        $('.back-to-top').on('click', function (e) {
-            e.preventDefault();
-            $('html,body').animate({
-                scrollTop: 0
-            }, 800);
-        });
+    /*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
     }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
+    
 
 })(jQuery);
